8d0c249919a4eadb2052067b8f70f3110cc4269d
Sahand Namvar <sahandnamvar97@gmail.com>
2025-07-18 16:03:03 -0400

    Setup Navbar component with animation

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import { motion } from "framer-motion";
+
+export const Hero = () => {
+  return (
+    <motion.section>
+      <div className="hero-container"></div>
+    </motion.section>
+  );
+};
